7b7f024343c2d10b30642fc78e763568ab7c3361
NimishMishra <neelam.nimish@gmail.com>
2024-06-21 14:09:03 +0530

    [tool] Fetch instructions.xml and profile instruction extensions on target system

---
+++ tool/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "[*] Fetching instructions.xml"
+wget https://uops.info/instructions.xml
+
+echo "[*] Listing available Instruction Set Extensions"
+echo "[*] Vector Extensions"
+lscpu | tr ' ' '\n' | grep avx
+
+echo "[*] SIMD Extensions"
+lscpu | tr ' ' '\n' | grep sse
+
+echo "[*] FMA Extensions"
+lscpu | tr ' ' '\n' | grep fma
+
+echo "[*] AES Extensions"
+lscpu | tr ' ' '\n' | grep aes
+
+echo "[*] BMI Extensions"
+lscpu | tr ' ' '\n' | grep bmi
+
+echo "[*] CET Extension"
+lscpu | tr ' ' '\n' | grep cet
+
+echo "[*] XSAVE Extensions"
+lscpu | tr ' ' '\n' | grep xsave
